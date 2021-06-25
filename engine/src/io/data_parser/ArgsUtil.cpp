afd4bc7cfd5213267c6b57f9eab3ff56bb155ede
ptaylor <paul.e.taylor@me.com>
2021-06-25 01:12:33 -0500

    fix compile error

--- engine/src/io/data_parser/ArgsUtil.cpp
+++ engine/src/io/data_parser/ArgsUtil.cpp
@@ -112,7 +112,7 @@ cudf::io::orc_reader_options getOrcReaderOptions(const std::map<std::string, std
 
 	cudf::io::orc_reader_options reader_opts = cudf::io::orc_reader_options::builder(cudf::io::source_info{&arrow_source});
 	if(map_contains("stripes", args)) {
-		reader_opts.set_stripes(to_vector_int(args.at("stripes")));
+		reader_opts.set_stripes({to_vector_int(args.at("stripes"))});
 	}
 	if(map_contains("skiprows", args)) {
 		reader_opts.set_skip_rows(to_int(args.at("skiprows")));
@@ -289,7 +289,7 @@ sql_info getSqlInfo(std::map<std::string, std::string> &args_map) {
     if (args_map.at("table_batch_size").empty()) {
       sql.table_batch_size = DETAULT_TABLE_BATCH_SIZE;
     } else {
-      sql.table_batch_size = static_cast<std::size_t>(std::atoll(args_map.at("table_batch_size").data())); 
+      sql.table_batch_size = static_cast<std::size_t>(std::atoll(args_map.at("table_batch_size").data()));
     }
   } else {
     sql.table_batch_size = DETAULT_TABLE_BATCH_SIZE;
