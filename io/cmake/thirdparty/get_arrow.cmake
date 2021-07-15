d9334917c1945e9b8b29f56b3cd5c4e72d24816d
ptaylor <paul.e.taylor@me.com>
2021-07-15 16:38:06 -0500

    remove CUDA_TOOLKIT_ROOT_DIR

--- io/cmake/thirdparty/get_arrow.cmake
+++ io/cmake/thirdparty/get_arrow.cmake
@@ -75,7 +75,6 @@ function(find_and_configure_arrow VERSION BUILD_STATIC ENABLE_S3 BUILD_ARROW_PYT
         GIT_SHALLOW     TRUE
         SOURCE_SUBDIR   cpp
         OPTIONS         "CMAKE_VERBOSE_MAKEFILE ON"
-                        "CUDA_TOOLKIT_ROOT_DIR ${CUDAToolkit_LIBRARY_ROOT}"
                         "CUDA_USE_STATIC_CUDA_RUNTIME ${CUDA_STATIC_RUNTIME}"
                         "ARROW_IPC ON"
                         "ARROW_CUDA ON"
